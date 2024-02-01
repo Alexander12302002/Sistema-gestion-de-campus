06192a061331d1f61f93c37db67c35e783984c8e
Alexander12302002 <106930173+Alexander12302002@users.noreply.github.com>
2024-01-31 21:10:15 -0300

    feat: :sparkles: CRUD de estudiantes de campus completado

--- modulos/camper.py
+++ modulos/camper.py
@@ -1,5 +1,5 @@
 import os
-from .variables import save, getAll
+from .variables import save, getAll, camper
 
 def create():
     os.system("cls")
@@ -11,18 +11,119 @@ def create():
     save({
         "Nombre": input("Ingrese el nombre del camper: "),
         "Apellido": input("Ingrese el apellido del camper: "),
-        "Edad": int(input("Ingrese la edad del camper: "))
+        "Edad": int(input("Ingrese la edad del camper: ")),
+        "Genero": input("Ingrese genero del camper: ")
             })
     os.system("pause")
 
-def read():
-    print(getAll())
+def read(codigo=None):
+    os.system("cls")
+    print("""
+        ********************
+        * Tabla del camper *
+        ********************
+    """)
+    if(codigo==None):
+        for i,val in enumerate(getAll()):
+            print(f"""
+            _______________________
+            Codigo: {i+1}
+            Nombre: {val.get("Nombre")}
+            Apellido: {val.get("Apellido")}
+            Edad: {val.get("Edad")}
+            Genero: {val.get("Genero")}
+            _________________________
+            """)
+    else:
+        val = getAll()[codigo-1]
+        print(f"""
+        _______________________
+        Codigo: {codigo}
+        Nombre: {val.get("Nombre")}
+        Apellido: {val.get("Apellido")}
+        Edad: {val.get("Edad")}
+        Genero: {val.get("Genero")}
+        _________________________
+        """)
+    os.system("pause")
+    
 
 def update():
-    print("El camper se actualizo")
+    os.system("cls")
+    read()
+    print("""
+        *************************
+        * Actualizar del camper *
+        *************************
+    """)
+    codigo = int(input("Cual es el codigo del camper que desea actualizar?: "))
+    read(codigo)
+    while(True):
+        print("""
+        Esta seguro que desea actualizar el camper?
+        1. si
+        2. no
+        3. cancelar
+        """)
+        opc = int(input(": "))
+        match(opc):
+            case 1:
+                val = {
+                    "Nombre": input("Ingrese el nombre del camper: "),
+                    "Apellido": input("Ingrese el apellido del camper: "),
+                    "Edad": int(input("Ingrese la edad del camper: ")),
+                    "Genero": input("Ingrese genero del camper: ")
+            }
+                camper[codigo-1] = val
+                print(f"""
+                    El camper fue Actualizado
+                    _________________________
+                    Codigo: {codigo}
+                    Nombre: {val.get("Nombre")}
+                    Apellido: {val.get("Apellido")}
+                    Edad: {val.get("Edad")}
+                    Genero: {val.get("Genero")}
+                    _________________________
+                    """)
+                os.system("pause")
+                break
 
 def delete():
-    print("El camper se borro")
+    os.system("cls")
+    read()
+    print("""
+        ***********************
+        * Eliminar del camper *
+        ***********************
+    """)
+    codigo = int(input("Cual es el codigo del camper que desea eliminar?: "))
+    read(codigo)
+    while(True):
+        print("""
+        Esta seguro que desea eliminar el camper?
+        1. si
+        2. no
+        3. cancelar
+        """)
+        opc = int(input(": "))
+        match(opc):
+            case 1: 
+                val = camper.pop(codigo-1)
+                os.system("cls")
+                print(f"""
+                El camper fue eliminado
+                _______________________
+                Codigo: {codigo}
+                Nombre: {val.get("Nombre")}
+                Apellido: {val.get("Apellido")}
+                Edad: {val.get("Edad")}
+                Genero: {val.get("Genero")}
+                _________________________
+                """)
+                os.system("pause")
+                break
+            case 3:
+                break
 
 def menu():
     menu = ["Guardar", "Buscar","Actualizar","Eliminar","Salir"]
